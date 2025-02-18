f4954aff421eca400cd22f645e2681b6e7547df6
Carlos Andrés Florez <caflorez@uniquindio.edu.co>
2025-02-18 18:22:36 -0500

    CRUD básico para Usuarios en memoria

---
+++ src/main/java/co/edu/uniquindio/proyecto/dto/EditarUsuarioDTO.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.proyecto.dto;
+
+public record EditarUsuarioDTO(
+    String id,
+    String nombre,
+    String ciudad,
+    String direccion,
+    String telefono
+) {
+}
